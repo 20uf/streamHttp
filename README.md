49872338411d587669ec691ff80835ec329c76a1
Michael COULLERET <michael@coulleret.pro>
2016-10-31 16:47:45 +0100

    [#initialize project] Establishment of architecture

---
+++ README.md
@@ -0,0 +1,8 @@
+streamHttp library
+==========
+
+The StreamHttp library defines an object-oriented layer for the HTTP specification.
+
+Resources
+---------
+
---
+++ src/ClientInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the StreamHttp project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace StreamHttp;
+
+/**
+ * Interface ClientInterface
+ *
+ * @author Michael COULLERET <michael@coulleret.pro>
+ */
+interface ClientInterface
+{
+}
